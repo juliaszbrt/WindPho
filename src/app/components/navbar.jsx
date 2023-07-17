d623b5cffcfda29306575865cf8e8f5e76c93e16
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2023-07-17 11:00:52 -0400

    fix error

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,7 +1,7 @@
 // 'use client'
 import Link from 'next/link';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
-import { useState } from "react"
+// import { useState } from "react"
 
 export default function Navbar() {
     // const [menuOpen, setMenuOpen] = useState(false)
